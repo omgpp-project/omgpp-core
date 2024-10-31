1c27af9b9bb90d67b639add4373da78ca1022c1e
Stas Bes <forgame98@yandex.ru>
2024-10-31 19:58:47 +0300

    Update main.rs

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,10 @@
+use gns::{GnsGlobal, GnsUtils};
+
 fn main() {
     println!("Hello, world!");
+    let gns_global = GnsGlobal::get().unwrap();
+    let gns_utils = GnsUtils::new().unwrap();
+
+    
 }
+
