af16fe49230799032c02cbff8a12328c455c455d
Stas Bes <forgame98@yandex.ru>
2024-11-01 16:51:21 +0300

    Added simple chat

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,10 +1,144 @@
-use gns::{GnsGlobal, GnsUtils};
+use std::{
+    io::stdin,
+    net::Ipv6Addr,
+    sync::mpsc,
+    thread,
+    time::{Duration, Instant},
+};
+
+use gns::{GnsGlobal, GnsSocket, GnsUtils, IsCreated};
+use gns_sys::{k_nSteamNetworkingSend_Reliable, k_nSteamNetworkingSend_Unreliable, ESteamNetworkingConnectionState};
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        panic!("Provide command line arguments. 1 - to start server, 2 - to start client")
+    }
+    let start_type = &args[1];
+    match &start_type[..] {
+        "1" => start_server(),
+        "2" => start_client(),
+        _ => panic!("error: invalid command"),
+    }
+}
+
+fn start_server() {
+    println!("Hello! Im Server");
     let gns_global = GnsGlobal::get().unwrap();
     let gns_utils = GnsUtils::new().unwrap();
 
-    
-}
+    let port: u16 = 55655;
+    let gns_socket = GnsSocket::<IsCreated>::new(&gns_global, &gns_utils).unwrap();
+    let server = gns_socket.listen(Ipv6Addr::LOCALHOST, port).unwrap();
 
+    // Now that we initiated a connection, there is three operation we must loop over:
+    // - polling for new messages
+    // - polling for connection status change
+    // - polling for callbacks (low-level callbacks required by the underlying library).
+    // Important to know, regardless of the type of socket, whether it is in [`IsClient`] or [`IsServer`] state, theses three operations are the same.
+    // The only difference is that polling for messages and status on the client only act on the client connection, while polling for messages and status on a server yield event for all connected clients.
+
+    /*
+        sequenceDiagram
+        engine->>+client: RunFrame()
+        client->>+ server: ReceiveNetworkData
+            server ->> server:ReceiveMessagesOnPollGroup
+
+        client->>+server: RunFrame()
+            server->>server:SteamGameServer_RunCallbacks
+            server->>server:SendDataToClients(with fixed FPS)
+
+        client->>-engine:
+    */
+    loop {
+        // Run the low-level callbacks.
+        server.poll_callbacks();
+
+        let _actual_nb_of_messages_processed = server.poll_messages::<128>(|message| {
+            println!("Msg income");
+            println!("{}", core::str::from_utf8(message.payload()).unwrap());
+        });
+
+        // Don't do anything with events.
+        // One would check the event for connection status, i.e. doing something when we are connected/disconnected from the server.
+        let _actual_nb_of_events_processed = server.poll_event::<128>(|event| {
+ 
+            match (event.old_state(), event.info().state()) {
+                // A client is about to connect, accept it.
+                (
+                  ESteamNetworkingConnectionState::k_ESteamNetworkingConnectionState_None,
+                  ESteamNetworkingConnectionState::k_ESteamNetworkingConnectionState_Connecting,
+                ) => {
+                  let _result = server.accept(event.connection());
+                }
+                _=>()
+            }
+            println!(
+                "Connection {}",
+                format!("{:?} {:?}", &event.info().state(), event.info().remote_address())
+            );
+        });
+
+        // send data to users with fixed FPS
+    }
+}
+fn start_client() {
+    println!("Hello! Im a client");
+    let (tx_channel, rx_channel) = mpsc::channel();
+    let _client_connection_thread = thread::spawn(move || {
+        let gns_global = GnsGlobal::get().unwrap();
+        let gns_utils = GnsUtils::new().unwrap();
+
+        let port: u16 = 55655;
+        let gns_socket = GnsSocket::<IsCreated>::new(&gns_global, &gns_utils).unwrap();
+        let client = gns_socket.connect(Ipv6Addr::LOCALHOST, port).unwrap();
+        let mut last_update = Instant::now();
+        let mut msg_buf = Vec::<String>::new();
+        loop {
+            client.poll_callbacks();
+
+            let _actual_nb_of_messages_processed = client.poll_messages::<128>(|message| {
+                println!("{}", core::str::from_utf8(message.payload()).unwrap());
+            });
+
+            let _actual_nb_of_events_processed = client.poll_event::<128>(|event| {
+                let conn = event.connection();
+                println!(
+                    "Connection Client {}",
+                    format!("{:?} {:?}", conn, event.info().remote_address())
+                );
+            });
+            let since_last_update = Instant::now() - last_update;
+            if since_last_update.as_millis() > 2000 {
+                last_update = Instant::now();
+                if msg_buf.len() > 0 {
+                    // take last messages and send
+                    for msg in &msg_buf{
+                        println!("Sent {}",msg);
+                        client.send_messages(vec![client.utils().allocate_message(
+                            client.connection(),
+                            k_nSteamNetworkingSend_Unreliable,
+                            msg.as_bytes(),
+                        )]);
+                    }
+                    msg_buf.clear();
+                }
+            }
+            loop {
+                if let Ok(received) = rx_channel.try_recv() {
+                    msg_buf.push(received);
+                }else {
+                    break;
+                }
+            }
+        }
+    });
+
+    // user input
+    loop {
+        let mut input = String::new();
+        _ = std::io::stdin().read_line(&mut input).expect("Some error");
+        tx_channel.send(input).unwrap();
+    }
+}
