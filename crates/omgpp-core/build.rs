015d2100c6aae1288290298f568e52c21ccdc148
Stas Bes <forgame98@yandex.ru>
2024-11-06 13:30:36 +0300

    feat: Added empty core library

---
+++ crates/omgpp-core/build.rs
@@ -0,0 +1,25 @@
+use std::{
+    path::{Path, PathBuf},
+    process::Command,
+};
+
+fn main() {
+    println!("cargo::rerun-if-changed=build.rs");
+    println!("cargo::rerun-if-changed=proto/general-message.proto");
+
+    println!("omgpp-core build started");
+
+    // Use this in build.rs
+    protobuf_codegen::Codegen::new()
+        // Use `protoc` parser, optional.
+        .protoc()
+        // Use `protoc-bin-vendored` bundled protoc command, optional.
+        // .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
+        // All inputs and imports from the inputs must reside in `includes` directories.
+        .includes(&["proto"])
+        // Inputs must reside in some of include paths.
+        .input("proto/general-message.proto")
+        // Specify output directory relative to Cargo output directory.
+        .cargo_out_dir("proto")
+        .run_from_script();
+}
---
+++ crates/omgpp-core/src/lib.rs
@@ -0,0 +1,7 @@
+
+#![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(dead_code)]
+
+include!(concat!(env!("OUT_DIR"), "/proto/general_message.rs"));
